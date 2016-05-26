31710bdec0a2be3c3b1ffc27d5627adbc2fd39fc
Lucas Melchior <switchboard.op@gmail.com>
2016-05-25 22:46:46 -0500

    tidying up

--- sketch.js
+++ sketch.js
@@ -1,43 +1,42 @@
 var data;
-var lineArray = ["hello","world"];
+var lineArray = ["hello", "world"];
 var time = 3;
 var rows = 30;
 
-function preload(){
+function preload() {
   data = loadStrings("data/pr0n.txt");
 }
 
 function setup() {
   //init lines
-  for (i = 0; i < rows ; i++){
+  for (i = 0; i < rows; i++) {
     lineArray.push(" ")
-    var lineId = "line" + (rows-(i+1)).toString();
+    var lineId = "line" + (rows - (i + 1)).toString();
     var line = createElement("pre", lineArray[i]);
     line.parent('#content').id(lineId);
-  } 
-  
-  for (i = 0; i <= data.length ; i++) {
-    printLine(data,i);
-    if ( i == data.length){i=0;}
+  }
+
+  for (i = 0; i <= data.length; i++) {
+    printLine(data, i);
   }
 }
 
 function draw() {}
 
-function printLine(data,i){
-  setTimeout(function () { 
-    
+function printLine(data, i) {
+  setTimeout(function () {
+
     //console.log(i + ": " + data[i]);
-    
+
     var newLine = data[i];
     lineArray.unshift(newLine);
     lineArray.pop();
-    
-    for (i=0; i < rows ;i++) {
-      
+
+    for (i = 0; i < rows; i++) {
+
       var lineSelector = "#line" + (i).toString();
       select(lineSelector).html(lineArray[i]);
-      
+
     }
-  }, time*i);
+  }, time * i);
 }
