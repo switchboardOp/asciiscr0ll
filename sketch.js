defc377fa4128b63bd4b238d32f38a9e2e7fa974
Lucas Melchior <switchboard.op@gmail.com>
2016-05-25 23:38:38 -0500

    looping feature built

--- sketch.js
+++ sketch.js
@@ -1,42 +1,40 @@
 var data;
-var lineArray = ["hello", "world"];
-var time = 3;
+var lineArray = [];
+var index = 0;
+
+//modify these as needed
 var rows = 30;
+var fr = 30;
+var lineheight = 15;
 
 function preload() {
   data = loadStrings("data/pr0n.txt");
 }
 
 function setup() {
+  frameRate(fr);
   //init lines
+  rows = displayHeight/lineheight;
   for (i = 0; i < rows; i++) {
     lineArray.push(" ")
     var lineId = "line" + (rows - (i + 1)).toString();
     var line = createElement("pre", lineArray[i]);
-    line.parent('#content').id(lineId);
-  }
-
-  for (i = 0; i <= data.length; i++) {
-    printLine(data, i);
+    line.parent('#scroll').id(lineId);
+    line.style("line-height", lineheight + "px");
   }
 }
 
-function draw() {}
+function draw() {
+    printLine(data, index);
+    index++;
+    if ( index >= data.length ) { index = 0; }
+}
 
 function printLine(data, i) {
-  setTimeout(function () {
-
-    //console.log(i + ": " + data[i]);
-
-    var newLine = data[i];
-    lineArray.unshift(newLine);
+    lineArray.unshift(data[i]);
     lineArray.pop();
-
     for (i = 0; i < rows; i++) {
-
       var lineSelector = "#line" + (i).toString();
       select(lineSelector).html(lineArray[i]);
-
     }
-  }, time * i);
 }
